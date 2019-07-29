7a109331b9b4c091811191364d3fdcac20c91661
MPCdevhub <mattcolwin@gmail.com>
2019-07-29 16:21:05 -0700

    delete script.js

--- Weight Converter/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('innrContainer').style.visibility = 'hidden';
-document.getElementById("wghtInput").addEventListener('input', function(ev) {
-    document.getElementById('innrContainer').style.visibility = 'visible';
-    var lbs = ev.target.value;
-    document.getElementById('gmOutput').innerHTML = lbs/0.0022046;
-    document.getElementById('kgOutput').innerHTML = lbs/2.2046;
-    document.getElementById('ozOutput').innerHTML = lbs * 16;
-
-});
